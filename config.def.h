390c458cd4fda4aebd9c4e4330ece0480b9f7aaf
Jonas Dujava <jonas.dujava@gmail.com>
2022-07-29 17:38:24 +0200

    fuzzy + colors + numbers

--- config.def.h
+++ config.def.h
@@ -2,16 +2,20 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"SauceCodePro Nerd Font Mono:size=10:antialias=true:autohint=true:weight=bold",
+	"JoyPixels:pixelsize=11:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                         fg          bg      */
+	[SchemeNorm]          = { "#3388cc", "#1e1e1e" },
+	[SchemeSel]           = { "#232323", "#2277cc" },
+	[SchemeSelHighlight]  = { "#ffff34", "#2277cc" },
+	[SchemeNormHighlight] = { "#ff87d7", "#1e1e1e" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
